8ec3277046ea9e265fce22358d917ce528fb5214
Jack T <jackmt9@gmail.com>
2020-01-21 17:23:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,12 +35,11 @@ end
 
 def reduce_to_total(source_array, starting_point = 0)
   i = 0 
-  array = []
   while source_array.length > i do
-    array[i] = source_array[i] + starting_point
+    starting_point = source_array[i] + starting_point
     i += 1 
   end
-  return array[-1]
+  return starting_point
 end
 
 def reduce_to_all_true(source_array)
