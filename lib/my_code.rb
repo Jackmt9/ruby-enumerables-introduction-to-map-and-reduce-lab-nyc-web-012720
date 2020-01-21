6df54fb2a2368b1e36bcb2ff91d36c75bd31e926
Jack T <jackmt9@gmail.com>
2020-01-21 17:20:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ def reduce_to_total(source_array, starting_point = 0)
     source_array[i] += starting_point
     i += 1 
   end
-  return starting_point
+  print starting_point
 end
 
 def reduce_to_all_true(source_array)
